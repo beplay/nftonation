206ede533433f9699de81376fe1f1ac02cda4efa
Thomas Kleb <thomas.kleb@ost.ch>
2022-10-27 13:37:12 +0200

    initial commit

--- wed2-template/controller/index-controller.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response} from "express";
-
-export class IndexController {
-    index(req: Request, res: Response) {
-        res.render("index", {data: ["Hello", "World", "!"], dark: false});
-    }
-}
-
-export const indexController = new IndexController();
