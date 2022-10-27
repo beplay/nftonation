5318ce7340a7eaa73f8ed5adf75a8c3112184baf
Thomas Kleb <thomas.kleb@ost.ch>
2022-10-27 15:52:03 +0200

    finished start page

---
+++ NFTonation/scripts/start-page.js
@@ -0,0 +1,34 @@
+// ---- METAMASK ELEMENTS ----
+const connectButton = document.getElementById("connect-button");
+const walletID = document.getElementById("wallet-id");
+const reloadButton = document.getElementById("reload-button");
+const installAlert = document.getElementById("install-alert");
+// ---- OTHER ELEMENTS ----
+const startPage = document.getElementsByClassName("start-page");
+const votePage = document.getElementsByClassName("vote-page");
+const votingPageButton = document.getElementById("vote-page-button");
+
+connectButton.addEventListener("click", () => {
+    if (typeof window.ethereum !== "undefined") {
+        ethereum
+            .request({ method: "eth_requestAccounts" })
+            .then((accounts) => {
+                const account = accounts[0]
+                walletID.innerHTML = `<p>Wallet connected: ${account}</p>`;
+            }).catch((error) => {
+            console.log(error, error.code);
+        });
+    } else {
+        window.open("https://metamask.io/download/", "_blank");
+        installAlert.classList.add("show");
+    }
+})
+
+reloadButton.addEventListener("click", () => {
+    window.location.reload();
+});
+
+votingPageButton.addEventListener("click", () => {
+    startPage[0].style.display = "none";
+    votePage[0].classList.remove("hidden-div");
+})
