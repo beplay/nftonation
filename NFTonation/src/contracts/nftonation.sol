36eb411d34e1249ffd1248f5791c0caca37063e4
Benjamin <benjamin.plattner@ost.ch>
2022-11-30 20:17:50 +0100

    add timer functionality

--- NFTonation/src/contracts/nftonation.sol
+++ NFTonation/src/contracts/nftonation.sol
@@ -6,17 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 
 interface iTON {
     function transfer(address to, uint256 tokenId) external;
     function showNFTBal(address owner) external view returns (uint256);
     function showOwnerOf(uint256 _tokenId) external view returns (address);
+    function burnTokenBurn(uint256 _tokenId) external;
 }
 
 // deploy TON first, then NFTonation
 
-contract TON is ERC721, Ownable, iTON {
+contract TON is ERC721, Ownable, ERC721Burnable, iTON {
     using Counters for Counters.Counter;
 
 
@@ -47,8 +49,12 @@ contract TON is ERC721, Ownable, iTON {
         return balanceOf(owner);
     }
 
-    function showOwnerOf(uint256 _tokenId) public view returns (address) {
+    function showOwnerOf(uint256 _tokenId) external view returns (address) {
         return ownerOf(_tokenId);
+    }
+
+    function burnTokenBurn(uint256 _tokenId) external {
+        burn(_tokenId);
     }
 
 }
@@ -67,12 +73,12 @@ contract NFTonation is IERC721Receiver {
         bool valid;
     }
 
-    mapping(address => Voter) public voters;
+    mapping(address => Voter) voters;
     mapping(address => Org) public orgs;
 
     address ownerAddr;
 
-    address[] public voter_addresses;
+    address[] voter_addresses;
     address[] public org_addresses;
 
     address org_receiver;
@@ -82,39 +88,64 @@ contract NFTonation is IERC721Receiver {
     bool voting_occurred = false;
 
     event TokenReceived(uint256);
-    event VoteSubmitted(string voted);
-    event VotingStarted(uint256);
-    event VotingDuration(uint256);
-    event WinnerToken(address);
+    event VoteSubmitted(string);
+    event VotingStarted(uint256, uint256);
+    event Winner(address, address, string, uint256);
+    event TokenBurned(uint256);
 
-    uint256 public startTime;
-    uint256 public durationMinutes;
+    uint256 startTime;
+    uint256 durationSeconds;
+    uint256 daysSet;
+    uint256 hoursSet;
+    uint256 minutesSet = 1; // by default 1 minute
+    uint256 secondsSet;
 
     uint256 tokenId;
 
     constructor(address tonation) {
         nft = iTON(tonation);
         ownerAddr = msg.sender;
+        setupOrgs();
+    }
+
+    function setupOrgs() internal {
         addOrg("WWF", address(0xbDA5747bFD65F08deb54cb465eB87D40e51B197E));
         addOrg("Unicef", address(0xdD2FD4581271e230360230F9337D5c0430Bf44C0));
         addOrg("Red Cross", address(0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199));
+    }
+
+    function setDuration(uint256 _days, uint256 _hours, uint256 _minutes, uint256 _seconds) public {
+        daysSet = _days;
+        hoursSet = _hours;
+        minutesSet = _minutes;
+        secondsSet = _seconds;
+    }
+
+    function resetNumVotes() internal {
+        for (uint256 i = 0; i < org_addresses.length; i++) {
+            orgs[org_addresses[i]].num_votes = 0;
+        }
+    }
+
+    function removeVoters() internal {
+        for (uint256 i = 0; i < voter_addresses.length; i++) {
+            delete(voters[voter_addresses[i]]);
+            delete(voter_addresses[i]);
+        }
+        voter_addr_length = 0;
     }
 
     function getBlockTime() public view returns (uint256) {
         return block.timestamp;
     }
 
-    function getOrgAddLength() external view returns (uint256) {
+    function getOrgAddrLength() external view returns (uint256) {
         return org_addr_length;
     }
 
-    function getVoterAddLength() external view returns (uint256) {
-        return voter_addr_length;
-    }
-
-
     function onERC721Received(address, address, uint256 _tokenId, bytes calldata) external returns (bytes4) {
         setTokenId(_tokenId);
+        startVoting();
         emit TokenReceived(_tokenId);
         return IERC721Receiver.onERC721Received.selector;
     }
@@ -123,13 +154,13 @@ contract NFTonation is IERC721Receiver {
         tokenId = _tokenId;
     }
 
-    function transferToken() public {
+    function transferToken() internal {
         requireVotingFinished();
         requireReceiverSet();
         nft.transfer(org_receiver, tokenId);
     }
 
-    function showNFTBal(address owner) public view returns (uint256) {
+    function showNFTBal(address owner) internal view returns (uint256) {
         return nft.showNFTBal(owner);
     }
 
@@ -137,15 +168,14 @@ contract NFTonation is IERC721Receiver {
         addVoter(msg.sender);
     }
 
-    function setReceiver(address _receiver) public {
-        requireOwner();
+    function setReceiver(address _receiver) internal {
         requireHasOrgAccount(_receiver);
         requireVotingFinished();
         org_receiver = _receiver;
     }
 
     function addVoter(address addr) internal {
-        requireNoAccount();
+        requireNoAccount(addr);
         voter_addresses.push(addr);
         voters[addr] = Voter(true, false);
         voter_addr_length += 1;
@@ -158,72 +188,69 @@ contract NFTonation is IERC721Receiver {
         org_addr_length += 1;
     }
 
-    function startVoting(uint256 _durationMinutes) public {
-        durationMinutes = _durationMinutes;
-        requireVoteNotStarted();
+    function startVoting() internal {
         requireContractTokenOwner();
+        requireVoteNotOccurred();
+
+        durationSeconds = convertToSeconds(daysSet, hoursSet, minutesSet, secondsSet);
+        requirePosVoteDuration();
+
+        removeVoters();
+        resetNumVotes();
+
         startTime = getBlockTime();
-        voting_occurred = false;
-        emit VotingStarted(startTime);
-        emit VotingDuration(_durationMinutes);
+
+        emit VotingStarted(startTime, durationSeconds);
     }
 
-    function resetVoteStatus() external {
-        voters[msg.sender].voted = false;
+    function convertToSeconds(uint256 _days, uint256 _hours, uint256 _minutes, uint256 _seconds) internal pure returns (uint256 secs) {
+        uint256 daysInSeconds = _days * 24 * 60 * 60;
+        uint256 hoursInSeconds = _hours * 60 * 60;
+        uint256 minutesInSeconds = _minutes * 60;
+        secs = daysInSeconds + hoursInSeconds + minutesInSeconds + _seconds;
+        return secs;
     }
 
-    function determineWinnerAddr() public view returns (address winner_addr) {
-        requireOwner();
-        requireVotingFinished();
-        (uint256 num_votes, string memory win_org) = getWinner();
-        if (num_votes == 0) {
-            winner_addr = address(0x0);
-        } else {
-            winner_addr = getOrgAddr(win_org);
-        }
-        return winner_addr;
-    }
-
-    function getWinnerVotes() public view returns (uint256 num_votes) {
-        requireVotingFinished();
-        (num_votes, ) = getWinner();
-        return num_votes;
-    }
-
-    function getWinner() internal view returns (uint256 max_vote, string memory win_org) {
+    function getWinner() internal view returns (uint256 max_vote, string memory win_org, address win_addr) {
         uint256 temp = 0;
         max_vote = 0;
         for (uint256 i = 0; i < org_addresses.length; i++) {
             temp = orgs[org_addresses[i]].num_votes;
             if (temp >= max_vote) {
                 if (temp == max_vote) {
-                    max_vote = 0;
                     win_org = "draw";
+                    win_addr = address(0x0);
                 } else {
                     max_vote = temp;
                     win_org = orgs[org_addresses[i]].name;
+                    win_addr = getOrgAddr(win_org);
                 }
             }
         }
-        return (max_vote, win_org);
+        return (max_vote, win_org, win_addr);
     }
 
     function finishVotingProcess() external {
         voting_occurred = true;
         requireVotingFinished();
 
-        address winner_addr = determineWinnerAddr();
-        requireAddrNotNull(winner_addr);
-        setReceiver(winner_addr);
-
-        transferToken();
-        address new_token_owner = nft.showOwnerOf(tokenId);
-        emit WinnerToken(new_token_owner);
+        (uint256 votes, string memory win_org, address winner_addr) = getWinner();
+        if (winner_addr == address(0x0)) {
+            nft.burnTokenBurn(tokenId);
+            emit TokenBurned(tokenId);
+        } else {
+            setReceiver(winner_addr);
+            transferToken();
+            address new_token_owner = nft.showOwnerOf(tokenId);
+            emit Winner(new_token_owner, winner_addr, win_org, votes);
+        }
+        voting_occurred = false;
     }
 
     function vote(string memory org_name) external {
         requireVotingOpen();
         requireNotVoted();
+
         addCurrentVoter();
 
         address org_addr = getOrgAddr(org_name);
@@ -240,11 +267,11 @@ contract NFTonation is IERC721Receiver {
         voters[msg.sender].voted = true;
     }
 
-    function getOrgAddr(string memory org_name) public view returns (address addr) {
+    function getOrgAddr(string memory _org_name) public view returns (address addr) {
         string memory name = "";
         for (uint256 i = 0; i < org_addresses.length; i++) {
             name = orgs[org_addresses[i]].name;
-            if (compareStrings(name, org_name)) {
+            if (compareStrings(name, _org_name)) {
                 addr = org_addresses[i];
             }
         }
@@ -259,17 +286,6 @@ contract NFTonation is IERC721Receiver {
             vote_count = string.concat(temp, orgs[org_addresses[i]].name, ":", Strings.toString(orgs[org_addresses[i]].num_votes), "-");
         }
         return vote_count;
-    }
-
-    function getAllVoters() external view returns (string memory entries) {
-        string memory temp = "";
-        for (uint256 i = 0; i < voter_addresses.length; i++) {
-            temp = entries;
-            string memory voted = "";
-            if (voters[voter_addresses[i]].voted) voted="voted"; else voted="not voted";
-            entries = string.concat(temp, Strings.toHexString(voter_addresses[i]), ":", voted, "-");
-        }
-        return entries;
     }
 
     function compareStrings(string memory s1, string memory s2) internal pure returns(bool){
@@ -289,10 +305,6 @@ contract NFTonation is IERC721Receiver {
         return orgs[addr].valid;
     }
 
-    function requireNoAccount() internal view {
-        require(!doesVoterExist(msg.sender) && !doesOrgExist(msg.sender), "You have already an account");
-    }
-
     function requireNoAccount(address _addr) internal view {
         require(!doesVoterExist(_addr) || !doesOrgExist(_addr), "You have already an account");
     }
@@ -308,17 +320,11 @@ contract NFTonation is IERC721Receiver {
     function requireVotingFinished() internal view {
         requirePosVoteDuration();
         require(voting_occurred == true, "Voting has not yet happened");
-        require(getBlockTime() > (startTime + durationMinutes * 1 minutes), "Voting still ongoing");
+        require(getBlockTime() > (startTime + durationSeconds * 1 seconds), "Voting still ongoing");
     }
 
     function requireVotingOpen() internal view {
-        require(getBlockTime() <= (startTime + durationMinutes * 1 minutes), "Voting inactive, maybe start it first");
-        requirePosVoteDuration();
-        requireVoteNotOccurred();
-        requireContractTokenOwner();
-    }
-
-    function requireVoteNotStarted() internal view {
+        require(getBlockTime() <= (startTime + durationSeconds * 1 seconds), "Voting inactive, maybe start it first");
         requirePosVoteDuration();
         requireVoteNotOccurred();
         requireContractTokenOwner();
@@ -329,7 +335,7 @@ contract NFTonation is IERC721Receiver {
     }
 
     function requirePosVoteDuration() internal view {
-        require(durationMinutes > 0, "Allow some time for voters to vote");
+        require(durationSeconds > 0, "Allow some time for voters to vote");
     }
 
     function requireAddrNotNull(address _addr) internal pure {
